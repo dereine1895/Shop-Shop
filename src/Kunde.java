71a5276f0d562f47b028b634358f5bc78762175b
Tom <tom@mvjt.de>
2026-01-20 12:46:08 +0100

    anfang binärbaum implementation

--- src/Kunde.java
+++ src/Kunde.java
@@ -1,6 +1,43 @@
 public class Kunde {
     private String Vorname;
     private String Nachname;
-    private String Kundennummer;
+    private int Kundennummer;
+    private List<Bestellung> bestellungen;
 
+    public Kunde() {
+
+    }
+
+
+    public String getVorname() {
+        return Vorname;
+    }
+
+    public void setVorname(String vorname) {
+        Vorname = vorname;
+    }
+
+    public String getNachname() {
+        return Nachname;
+    }
+
+    public void setNachname(String nachname) {
+        Nachname = nachname;
+    }
+
+    public int getKundennummer() {
+        return Kundennummer;
+    }
+
+    public void setKundennummer(int kundennummer) {
+        Kundennummer = kundennummer;
+    }
+
+    public List<Bestellung> getBestellungen() {
+        return bestellungen;
+    }
+
+    public void setBestellungen(List<Bestellung> bestellungen) {
+        this.bestellungen = bestellungen;
+    }
 }
